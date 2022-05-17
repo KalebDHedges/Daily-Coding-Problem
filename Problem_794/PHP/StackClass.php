65ce8a288ddd1e808dd74f34ed98cfe9fc95a427
KalebDHedges <63331865+KalebDHedges@users.noreply.github.com>
2022-05-17 14:50:06 +1000

    added StackClass.php

---
+++ Problem_794/PHP/StackClass.php
@@ -0,0 +1,21 @@
+<?php
+
+  class Stack {
+    private $elements;
+
+    public Stack() {}
+
+    public function push($newElement) {
+      // NOT IMPLEMENTED
+    }
+
+    public function pop() {
+      // NOT IMPLEMENTED
+    }
+
+    public function max() {
+      // NOT IMPLEMENTED
+    }
+  }
+
+?>
